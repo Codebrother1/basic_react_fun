394ffa74b08192ae433d57b54e3534a1af7bf53e
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-04-04 09:30:33 -0500

    conditional rendering short circuit rendering react

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -10,15 +10,29 @@ class UserGreeting extends Component {
   }
   
   render() {
-    if (this.state.isLoggedIn){
-      return(
-        <div>Welcome TC</div>
-      )
-    } else {
-      return (
-        <div>Welcome Guest</div>
-      )
-    }
+    return  this.state.isLoggedIn && <div>Welcome TC</div>
+    // return this.state.isLoggedIn ? (
+    //   <div>Welcome TC</div>
+    // ) : (
+    //   <div>Welcome Guest</div>
+    // )
+    // let message
+    // if (this.state.isLoggedIn){
+    //   message = <div>Welcome TC</div>
+    // } else {
+    //   message = <div>Welcome Guest</div>
+    // }
+
+    // return <div>{message}</div>
+    // if (this.state.isLoggedIn){
+    //   return(
+    //     <div>Welcome TC</div>
+    //   )
+    // } else {
+    //   return (
+    //     <div>Welcome Guest</div>
+    //   )
+    // }
     // return (
     //   <div>
     //   <div>Welcome TC</div>
